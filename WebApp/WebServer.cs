a640fa160e172101f0051c5a08fb6a0211121a55
schneim <marco.schneider@tui.de>
2017-08-03 13:30:16 +0200

    WebServer Test

---
+++ WebApp/WebServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+
+        public WebServer()
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
+
+            _listener.Prefixes.Add("http://*:8080/");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Starting Webserver");
+            WebServer webServer = new WebServer();
+            webServer.Start();
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+            while(true)
+            {
+                HttpListenerContext ctx = _listener.GetContext();
+                new Thread(new Worker(ctx).ProcessRequest).Start();
+            }
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+
+    }
+}
---
+++ WebApp/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Worker
+    {
+        private HttpListenerContext context;
+
+        public Worker(HttpListenerContext context)
+        {
+            this.context = context;
+        }
+
+        public void ProcessRequest()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<html><body><h1>Hello World from C# and Docker</h1>");
+
+            byte[] b = Encoding.UTF8.GetBytes(sb.ToString());
+            context.Response.ContentLength64 = b.Length;
+            context.Response.OutputStream.Write(b, 0, b.Length);
+            context.Response.OutputStream.Close();
+        }
+    }
+}
