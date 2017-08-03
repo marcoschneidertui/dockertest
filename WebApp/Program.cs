e9205e6fb92be2bc6e8b2ba578098efe62c7957b
schneim <marco.schneider@tui.de>
2017-08-03 15:33:49 +0200

    New program behavior

---
+++ WebApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    class Program
+    {
+        public Program()
+        {
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Starting Application");
+            Console.WriteLine("I will do nothing else than writing a console output 100 times");
+            for(int i=0; i < 100; i++)
+            {
+                Console.WriteLine($"Now writing line {i+1}");
+            }
+        }
+
+    }
+}
